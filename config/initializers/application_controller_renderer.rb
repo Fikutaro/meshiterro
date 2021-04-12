29ace6c3259aa32d57590d69b8c1bcb2409b844b
Fikutaro <nomio9029@gmail.com>
2021-04-12 11:06:39 +0000

    [Finish]4-3

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '83d67017487778bf84a3f5bd0370e19a8930191dd57444b6033c4fed5f444bfdcee21d275e3040293ea9a1b7d30474752eb48fdbf635046f65315da7a0872b52'
+
